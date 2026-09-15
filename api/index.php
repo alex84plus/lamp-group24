5a41aaa2849ab4cdb8644f0d64ddf6a1cb2105cc
aidan-oien <oien313@gmail.com>
2026-09-15 17:44:47 -0400

    connect signup api to frontend

--- api/index.php
+++ api/index.php
@@ -45,45 +45,40 @@ if ($method === 'GET' &&
 if ($method === 'POST')
 {
     $body = getRequestBody();
-    if ( isset( $body['signup'] ) &&
-         isset( $body['login'] ) &&
+    if ( isset( $body['firstName'] ) &&
+         isset( $body['lastName'] ) &&
          isset( $body['password'] ) &&
-         isset( $body['firstName'] ) &&
-         isset( $body['lastName'] ) )
+         isset( $body['login'] ) )
     {
+        $login     = clean( $body['login'] );
+        $password  = clean( $body['password'] );
+        $firstName = clean( $body['firstName'] );
+        $lastName  = clean( $body['lastName'] );
 
-        if ( $body['signup'] )
+        //check all our data is actually defined
+        if ( !$login || !$password || !$firstName || !$lastName )
         {
-            $login     = clean( $body['login'] );
-            $password  = clean( $body['password'] );
-            $firstName = clean( $body['firstName'] );
-            $lastName  = clean( $body['lastName'] );
+            respond( 400, ['error' => 'Login, Password, First Name, and Last Name are required'] );
+        }
 
-            //check all our data is actually defined
-            if ( !$login || !$password || !$firstName || !$lastName )
-            {
-                respond( 400, ['error' => 'Login, Password, First Name, and Last Name are required'] );
-            }
+        //check to see if user already exists
+        $stmt = $db->prepare('SELECT ID FROM Users WHERE Login = :login LIMIT 1');
+        $stmt->execute( [':login' => $login] );
+        $user = $stmt->fetch();
 
-            //check to see if user already exists
-            $stmt = $db->prepare('SELECT ID FROM Users WHERE Login = :login LIMIT 1');
-            $stmt->execute( [':login' => $login] );
-            $user = $stmt->fetch();
+        // if user exists, send 403 Forbidden, if not, create a new account with 201 Created
+        if ($user)
+        {
+            respond( 403, ['error' => 'Username Taken'] );
+        }
+        else
+        {
+            //create new account
+            $stmt = $db->prepare( 'INSERT INTO Users (FirstName,LastName,Login,Password)
+                                    VALUES (:firstName, :lastName, :login, :password);' );
+            $stmt->execute( [':firstName' => $firstName, ':lastName' => $lastName, ':login' => $login, ':password' => $password ] );
 
-            // if user exists, send 403 Forbidden, if not, create a new account with 201 Created
-            if ($user)
-            {
-                respond( 403, ['error' => 'Username Taken'] );
-            }
-            else
-            {
-                //create new account
-                $stmt = $db->prepare( 'INSERT INTO Users (FirstName,LastName,Login,Password)
-                                       VALUES (:firstName, :lastName, :login, :password);' );
-                $stmt->execute( [':firstName' => $firstName, ':lastName' => $lastName, ':login' => $login, ':password' => $password ] );
-
-                respond( 201, ['error' => ''] );
-            }
+            respond( 201, ['error' => ''] );
         }
     }
 }
